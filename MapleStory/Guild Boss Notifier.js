9155301f75ce1f48b33aafc211192ae7f110488e
Potato-Y <cg2358@naver.com>
2020-07-15 15:02:48 +0900

    Update Guild Boss Notifier.js
    bug fix

--- MapleStory/Guild Boss Notifier.js
+++ MapleStory/Guild Boss Notifier.js
@@ -5,24 +5,25 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
 	var c=0;
 	if(msg=="on"){
 		if(room=="길드 방 이름"){ //필요시 없애도 됨
-		replier.reply("길보 알리미 서비스를 시작합니다.");
-		var s="on";
-		while(s=="on"){
-			var time=new Date();
-			if(time.getHours()==12||time.getHours()==20){
-				if(c==0){
-					replier.reply("길보돌 시간입니다");
-					c++;
-				}
+			replier.reply("길보 알리미 서비스를 시작합니다.");
+			var s="on";
+			while(s=="on"){
+				var time=new Date();
+				if(time.getHours()==12||time.getHours()==20){
+					if(c==0){
+						replier.reply("길보돌 시간입니다");
+						c++;
+					}
 				
-			}
-			if(time.getHours()==13||time.getHours()==21){
+				}
+				if(time.getHours()==13||time.getHours()==21){
 					if(c==1){
 						replier.reply("길보 종료 시간입니다");
 						c++;
 						c=0;
 					}
 				}
+			}
 		}
 	}
 }
