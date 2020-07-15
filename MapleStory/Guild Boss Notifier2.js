a5210be0b3db91c49a186bc6a653b89a8a350a93
Potato-Y <cg2358@naver.com>
2020-07-15 15:15:55 +0900

    Create Guild Boss Notifier2.js

---
+++ MapleStory/Guild Boss Notifier2.js
@@ -0,0 +1,41 @@
+var c;
+var use;
+
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+	msg=msg.trim();
+	sender=sender.trim();
+	room=room.trim();
+	
+	if(msg=="!알림"){
+		if (use==undefined||use==2){
+			use=1;
+			replier.reply("길드 보스 알리미를 시작합니다.");
+		}
+		else if(use==1){
+			use=2;
+			replier.reply("길드 보스 알리미를 종료합니다.");
+		}
+	}
+	
+	while(use==1){
+		check();
+	}
+	
+	function check(){
+		var time=new Date();
+		if(time.getHours()==12||time.getHours()==20){
+			if(c==0){
+				replier.reply("길보돌 시간입니다");
+				c++;
+				}
+			
+			if(time.getHours()==13||time.getHours()==21){
+				if(c==1){
+					replier.reply("길보 종료 시간입니다");
+					c++;
+					c=0;
+				}
+			}
+		}
+	}
+}
